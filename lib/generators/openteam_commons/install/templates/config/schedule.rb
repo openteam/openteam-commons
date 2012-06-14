483b696e28b66f27d5d24649ef5a311770153ca7
Dmitry Lihachev <lda@openteam.ru>
2012-06-14 14:04:07 +0700

    rvm support

--- lib/generators/openteam_commons/install/templates/config/schedule.rb
+++ lib/generators/openteam_commons/install/templates/config/schedule.rb
@@ -1,4 +1,8 @@
-set :job_template, "/usr/local/bin/bash -l -c ':job'" if RUBY_PLATFORM =~ /freebsd/
+if RUBY_PLATFORM =~ /freebsd/
+  set :job_template, "/usr/local/bin/bash -l -i -c ':job'"
+else
+  set :job_template, "/bin/bash -l -i -c ':job'"
+end
 
 every :day do
   rake 'cron'
